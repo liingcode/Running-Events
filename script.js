08d2e72e0ed5c9da782c02c90f133958bc9bce61
DASHIELL LOCKLEAR <dashiell.locklear@gmail.com>
2021-06-28 05:36:15 -0700

    edits

---
+++ script.js
@@ -0,0 +1,16 @@
+'use strict'
+document.write('this is script');
+
+let answer = prompt ('would you like to join our email list?');
+
+
+if (answer === 'yes') {
+    let emailAddress = prompt ('Enter your email address below');
+    
+}
+
+
+//The user get's prompted with a question and they input a
+// yes or no answer that may prompt them again based on the
+// first answer. 
+
