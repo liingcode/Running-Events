9f34cca62ea0bb3eab44b1a9cc3a0205a783aea5
DASHIELL LOCKLEAR <dashiell.locklear@gmail.com>
2021-07-01 17:54:22 -0700

    added for loop and while loop

--- script.js
+++ script.js
@@ -23,7 +23,34 @@ function sayHello() {
     let firstname = prompt('hello, what is your first name?');
     let lastname = prompt('What is your lastname?');
     console.log(firstname + ' ' + lastname);
-document.write(firstname +' '+ lastname);
+    document.write(firstname + ' ' + lastname);
 
 
 }
+
+
+
+//ask the question, how many feet in a mile? keep asking until 5,280 
+//is entered. 
+
+function howManyFeet() {
+    let feetMiles = prompt('How many feet in a mile?');
+    while (feetMiles !== '5,280') {
+        feetMiles = prompt('That is not correct. Try again. How many feet in a mile?');
+
+    }
+
+
+
+    //have a shoe question prompt that writes the number of shoes in pictures on the page 
+
+    let shoeCounter = prompt('How many pairs of running shoes do you own?');
+
+    for (let i = 0; i < shoeCounter; i++) {
+        document.write('<img class="shoesPic" src="pairshoes.jpg"/>');
+    }
+}
+
+
+
+
